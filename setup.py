1db600d54d8ec28ad258cbe244c28d28785d3095
Christian Sachs <christian.sachs@sachsmail.de>
2021-10-27 16:45:48 +0200

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydupe',
-    version='1.0.1',
+    version='1.0.2',
     author = 'C. Sachs',
     author_email = 'python@sachsmail.de',
     description = 'A Package to deal with dupes',
